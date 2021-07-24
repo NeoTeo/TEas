0cae80af70da773cd8901e6a6bd38ca3a4cc2756
NeoTEo <m@sartori.dk>
2021-07-24 16:57:40 +0200

    Change to read lines. Add cleanup.

--- teco.c
+++ teco.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int
@@ -8,15 +9,25 @@ main(int argc, char** argv) {
 	else {
 		char* filepath = argv[2];	
 		FILE *f;
-		char ch;
+		char* sourceline = NULL;
+		size_t len = 0;
+		ssize_t read;
+		const char* strlit = "6 7 + pop";
 
 		if(!(f = fopen(filepath, "rb"))) {
 			fprintf(stderr, "Error: source file missing\n");
 			return 0;
 		}
-		while((ch = fgetc(f)) != EOF) {
-			printf("%c",ch);
+		//while((ch = fgetc(f)) != EOF) {
+		while((read = getline(&sourceline, &len, f) != EOF)) {
+			printf("%s",sourceline);
 		}
+
+
+		// clean up.
+		fclose(f);
+		if(sourceline) free(sourceline);
 		printf("done");
+		exit(EXIT_SUCCESS);
 	}
 }
