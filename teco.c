85db40dccc1ef226d96da889348c9cab5983bb40
NeoTEo <m@sartori.dk>
2021-07-24 18:22:03 +0200

    Add labels and tokenization of lines.

--- teco.c
+++ teco.c
@@ -12,7 +12,7 @@ main(int argc, char** argv) {
 		char* sourceline = NULL;
 		size_t len = 0;
 		ssize_t read;
-		const char* strlit = "6 7 + pop";
+		char* token;
 
 		if(!(f = fopen(filepath, "rb"))) {
 			fprintf(stderr, "Error: source file missing\n");
@@ -20,7 +20,11 @@ main(int argc, char** argv) {
 		}
 		//while((ch = fgetc(f)) != EOF) {
 		while((read = getline(&sourceline, &len, f) != EOF)) {
-			printf("%s",sourceline);
+			printf(":========= %s",sourceline);
+			while((token = strsep(&sourceline, " "))) {
+				printf("token %s\n", token);
+						
+			}
 		}
 
 
@@ -30,4 +34,32 @@ main(int argc, char** argv) {
 		printf("done");
 		exit(EXIT_SUCCESS);
 	}
+}
+
+typedef unsigned short Uint16;
+
+typedef struct Label {
+	char* label;
+	short addr;
+} Label;
+
+Label labels[256];
+int lcount = 0;
+
+void
+addLabel(Label label) {
+	if(lcount >= sizeof(labels)/sizeof(labels[0])) {
+		printf("Error: label buffer full!");
+		return;
+	}	
+	// do i need to copy the label string here?
+	labels[lcount++] = label;
+}
+
+int
+isWord(char* token) {
+	for (int i=0;i<lcount;i++) {
+		if(strcmp(token, labels[i].label) == 0) return 1;
+	}
+	return 0;
 }
